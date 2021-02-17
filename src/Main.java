4c5c9eea91927e928a7040c85b378c9711480b88
Anastasia <nastenaprokopeva78@gmail.com>
2021-02-17 23:38:39 +0300

    BMI

--- src/Main.java
+++ src/Main.java
@@ -1,5 +1,14 @@
 public class Main {
     public static void main(String[] args) {
-        System.out.println(50/(1.5*1.5));
+        int weightKg = 50;
+        double heightM = 1.5;
+        double bodyMassIndex = weightKg / (heightM * heightM);
+        System.out.println(bodyMassIndex);
+        System.out.println("BMI VALUES");
+        System.out.println("Underweight: under 18.5");
+        System.out.println("Normal: 18.5-25");
+        System.out.println("Overweight: 25-30");
+        System.out.println("Obesity: 30 or more");
+
     }
 }
