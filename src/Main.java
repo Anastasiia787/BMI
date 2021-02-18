6ea0ad2050e16f86956159094aa02540ac57183d
Anastasia <nastenaprokopeva78@gmail.com>
2021-02-18 22:25:08 +0300

    BMI

--- src/Main.java
+++ src/Main.java
@@ -3,12 +3,12 @@ public class Main {
         int weightKg = 50;
         double growthM = 1.5;
         double bodyMassIndex = weightKg / (growthM * growthM);
-        System.out.println(bodyMassIndex);
-        System.out.println("BMI VALUES:");
+
         System.out.println("Underweight: under 18.5");
         System.out.println("Normal: 18.5-25");
         System.out.println("Overweight: 25-30");
         System.out.println("Obesity: 30 or more");
 
+        System.out.println("BMI VALUES: " + bodyMassIndex);
     }
 }
