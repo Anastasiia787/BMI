008a2007d65a194c301b14c4ce31ee8971bf78d1
Anastasia <nastenaprokopeva78@gmail.com>
2021-02-19 22:48:51 +0300

    height in sm

--- src/Main.java
+++ src/Main.java
@@ -1,14 +1,18 @@
 public class Main {
     public static void main(String[] args) {
         int weightKg = 50;
-        double growthM = 1.5;
-        double bodyMassIndex = weightKg / (growthM * growthM);
+        double heightSm = 150;
+        double heightM = heightSm/100;
+        double bodyMassIndex = weightKg / (heightM * heightM);
 
+        System.out.println("BMI VALUES:");
         System.out.println("Underweight: under 18.5");
         System.out.println("Normal: 18.5-25");
         System.out.println("Overweight: 25-30");
         System.out.println("Obesity: 30 or more");
 
-        System.out.println("BMI VALUES: " + bodyMassIndex);
+        System.out.printf("Your BMI: %.2f ", bodyMassIndex);
+
+
     }
 }
